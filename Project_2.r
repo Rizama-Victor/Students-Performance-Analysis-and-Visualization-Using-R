26f7571cbd69ac6dab398bc720c22cad9da5d495
Rizama Victor <105273052+Rizama-Victor@users.noreply.github.com>
2025-11-15 18:35:31 +0100

    Update Project_2.r

--- Project_2.r
+++ Project_2.r
@@ -8,7 +8,8 @@
 # e calculate the deviation from mean of each parental level of education (i.e total score- average)
 
 # f plot the average of math score by the by parental level of education
-# g plot the average of total score of each student by the by parental level of education
+# g plot the total math score based on parental level of education
+# h plot the race ethnicity math score 
 
 ## NOTE: 
 # 1 Kindly include comments
@@ -133,5 +134,6 @@ mean_dev_for_each_parental_lvl <- student_performance_df %>%
   summarise((mean_dev = sum(each_student_total_score) - mean(each_student_total_score)))  # -- deviation from mean for each parental level of education
 
 View(mean_dev_for_each_parental_lvl) # -- displays the dataframe
+
 
 
